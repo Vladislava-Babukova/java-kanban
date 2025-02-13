80b6730a9b168267186cfe0eab4353350e2c19f6
Vladislava Babukova <Vlada.409b@gmail.com>
2025-02-13 18:49:46 +0300

    Финальное задание 6 спринта

--- src/test/java/InMemoryHistoryManagerTest.java
+++ src/test/java/InMemoryHistoryManagerTest.java
@@ -14,7 +14,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-class inMemoryHistoryManagerTest {
+class InMemoryHistoryManagerTest {
     HistoryManager historyManager;
 
     @BeforeEach
